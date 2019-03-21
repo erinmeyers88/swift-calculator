cf3583b36879b2ecf7c603ec4d584875c77e3e61
Vincent, Erin <evincent@ariit.com>
2019-03-21 11:07:52 -0600

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Erin Vincent on 3/21/19.
+//  Copyright © 2019 Erin Vincent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
